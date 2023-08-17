87ffaced72f6e075803dcaf00907248e14d73f52
Omar Afifi <131987634+omarafifi05@users.noreply.github.com>
2023-08-17 19:28:39 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,90 @@
+// html elements
+const background = document.querySelector("body")
+const text = document.querySelectorAll(".paragraphe")
+const navba = document.querySelectorAll(".lastitem")
+const botona = document.querySelector(".switchmodeimg")
+const cv = document.querySelector(".download")
+const cv2 = document.querySelector(".kany")
+const moon = document.querySelector(".moon")
+// dark mode logic
+
+
+
+let darkMode = false
+
+function switchDarkMode(){
+   
+    if(darkMode == false){
+        darkMode = true
+       background.style.backgroundColor = "black"
+
+      botona.style.backgroundColor = "black"
+      botona.style.color = "white"
+       
+
+      cv.style.backgroundColor = "white"
+      cv2.style.color = "black"
+    
+    
+
+
+
+moon.src="sun.png";
+for(let i=0; i<navba.length; i++){
+    navba[i].style.color = "#3498db"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+       for(let i=0; i<navba.length; i++){
+        navba[i].style.color = "white"
+   }
+
+
+       for(let i=0; i<text.length; i++){
+            text[i].style.color = "white"
+       }
+
+
+    } else{
+        darkMode = false
+        background.style.backgroundColor = "white"
+        botona.style.backgroundColor = "white"
+        botona.style.color = "black"
+      
+
+        cv.style.backgroundColor = "black"
+      cv2.style.color = "white"
+
+
+      moon.src="moon.png";
+      for(let i=0; i<navba.length; i++){
+          navba[i].style.color = "#3498db"
+      }
+      
+
+
+        for(let i=0; i<text.length; i++){
+            text[i].style.color = "black"
+       }
+
+
+       for(let i=0; i<navba.length; i++){
+        navba[i].style.color = "black"
+   }
+
+    }
+    console.log(darkMode)
+}
+
+switchDarkMode()
+
